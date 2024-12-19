cc15c87a1ddde86f08bf1101105e05d7b8f2684e
Mann Jaiswal <mannjaiswal5@gmail.com>
2024-12-19 10:45:34 -0700

    added Mongoose Population

--- routes/users.js
+++ routes/users.js
@@ -11,24 +11,31 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/signup', (req, res)=> {
-User.register(
-  new User({username: req.body.username}),
-  req.body.password,
-  err => {
-    if (err) {
-      res.statusCode = 500;
+  const user = new User({username: req.body.username});
+
+User.register(user,
+  req.body.password)
+  .then(registeredUser => {
+    if (req.body.firstName){
+      registeredUser.firstName = req.body.firstname;
+    }
+    if (req.body.lastName) {
+      registyeredUser.lastName = req.body.lastname;
+    }
+    return registeredUser.save();
+  })
+  .then(() => {
+    passport.authenticate('local')(req, res, () => {
+      res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
-      res.json({err: err});
-    }
-    else {
-      passport.authenticate('local')(req, res, () => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({success: true, status: 'Registration Successfull'});
-      });
-    }
-  }
-);
+      res.json({success: true, status: 'Registration Successful'});
+    });
+  })
+  .catch(err=> {
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({err: err});
+  });
 });
 
 router.post('/login', passport.authenticate('local', {session: false}), (req, res)=> {
